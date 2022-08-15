34e97985694f4d49599c733264db4ccf06bb05d0
cookieukw <erdesouzapereira@gmail.com>
2022-08-14 22:29:48 -0300

    arrumando bugs e erros e fazendo alterações

--- mysql.js
+++ mysql.js
@@ -1,11 +1,11 @@
-const sql = require("mysql")
+const sql = require("mysql");
 
 let pool = sql.createPool({
-"user": process.env.MYSQL_USER || "root",
-"password":process.env.MYSQL_PASSWORD || "root",
-"database":process.env.MYSQL_DATABASE || "sla",
-"host":process.env.MYSQL_HOST || "localhost",
-"port": process.env.MYSQL_DB_PORT || 3306
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "",
+  database: process.env.MYSQL_DATABASE || "library",
+  host: process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_DB_PORT || 3306,
 });
 
 exports.pool = pool;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const app = require('./index');
+const http = require("http");
+const app = require("./app");
 const port = process.env.MYSQL_APP_PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
