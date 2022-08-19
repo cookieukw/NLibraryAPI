bf03ae8b7f9d89a7c62b1c7eb0a09c0cd6239530
cookieukw <erdesouzapereira@gmail.com>
2022-08-19 20:57:28 -0300

    add limit to limit size

--- controllers/image/get_db.js
+++ controllers/image/get_db.js
@@ -5,21 +5,25 @@ exports.getDB = async(req, res)=>{
         page: parseInt(req.query.page, 10) || 0,
         limit: parseInt(req.query.limit, 10) || 10
     }
-
+ if(pageOptions.limit >= 21) return res.status(416).json({ message: "The maximum size of items is 20"});
 try {
      const allData = await imgModel.find()
     .skip(pageOptions.page * pageOptions.limit) 
     .limit(pageOptions.limit)
-    .sort("_id").exec((err, events)=> {
-        if(err) return res.status(404).json({ message: "An error has occurred :("});
+    .select("-_id")
+    .select("-__v")
+    .select("-_id/$oid")
+    .sort([['date', -1]])
+    .exec((err, events)=> {
+        if(err) return res.status(404).json({ message: "An error has occurred on search :("});
 
         imgModel.countDocuments().exec((err, count)=> {
-            if(err) return res.status(404).json({ error: "An error has occurred :("});
+            if(err) return res.status(404).json({ error: "An error has occurred on count :("});
             res.status(200).json(events)
         })
     })
 } catch (error) {
-    res.status(404).json({ message: "An error has occurred :("});
+    res.status(404).json({ message: "An error has occurred on get method:("});
 }
 
 }
